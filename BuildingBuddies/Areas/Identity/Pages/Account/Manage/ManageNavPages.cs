27280cf1be57ce082a8dbc9fc79401c8e1bf93fd
Jurica Vlahoviček <jurica.vlahovicek@gmail.com>
2018-12-01 15:54:44 +0100

    Revert "test za revert"
    This reverts commit ebca4daeb77a83cf3ef9b5877c0fc5ae46b91e0e.

---
+++ BuildingBuddies/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BuildingBuddies.Areas.Identity.Pages.Account.Manage
+{
+    public static class ManageNavPages
+    {
+        public static string ChangePassword => "ChangePassword";
+        
+        public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+
+        public static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
